282242d2e39748cf7147241524e53d4403927a33
Marveeeen <jmvroqueee@gmail.com>
2025-09-10 09:54:35 +0800

    add basic endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 import express from "express";
+import { handlerReadiness } from "./handler_readiness.js";
+import { middlewareLogResponse, middlewareMetricsInc } from "./middleware.js";
+import { handlerMetrics } from "./metrics.js";
+import { handlerReset } from "./reset.js";
+import { handlerChirpsValidate } from "./chirps.js";
 
-const PORT = 8080;
 const app = express();
+const PORT = 8080;
+
+// Built-in JSON body parsing middleware
+app.use(express.json());
+app.use(middlewareLogResponse);
+
+app.use("/app", middlewareMetricsInc, express.static("./src/app"));
+
+app.get("/admin/metrics", handlerMetrics);
+app.post("/admin/reset", handlerReset);
+app.get("/api/healthz", handlerReadiness);
+app.post("/api/validate_chirp", handlerChirpsValidate);
 
 app.listen(PORT, () => {
-  console.log(`Listening to PORT ${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
