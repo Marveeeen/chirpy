983a25640a1262c3c96e4f8eac1dae2b0fffc5ec
Marveeeen <jmvroqueee@gmail.com>
2025-09-11 13:01:30 +0800

    add create users and reset users

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
 import express from "express";
+import postgres from "postgres";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import { drizzle } from "drizzle-orm/postgres-js";
+
 import { handlerReadiness } from "./handler_readiness.js";
+import { handlerMetrics } from "./metrics.js";
+import { handlerReset } from "./reset.js";
+import { handlerChirpsValidate } from "./chirps.js";
 import {
   errorMiddleWare,
   middlewareLogResponse,
   middlewareMetricsInc,
 } from "./middleware.js";
-import { handlerMetrics } from "./metrics.js";
-import { handlerReset } from "./reset.js";
-import { handlerChirpsValidate } from "./chirps.js";
+
+import { config } from "./config.js";
+import { handlerUsersCreate } from "./users.js";
+
+const migrationClient = postgres(config.db.url, { max: 1 });
+await migrate(drizzle(migrationClient), config.db.migrationConfig);
 
 const app = express();
-const PORT = 8080;
 
 // Built-in JSON body parsing middleware
 app.use(express.json());
@@ -32,8 +41,12 @@ app.post("/api/validate_chirp", (req, res, next) => {
   Promise.resolve(handlerChirpsValidate(req, res)).catch(next);
 });
 
+app.post("/api/users", (req, res, next) => {
+  Promise.resolve(handlerUsersCreate(req, res)).catch(next);
+});
+
 app.use(errorMiddleWare);
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(config.api.port, () => {
+  console.log(`Server is running at http://localhost:${config.api.port}`);
 });
